9f8def9579def860257065b65bfc78eecf47712f
lei xu <komushi@gmail.com>
2015-01-08 08:52:04 +0900

    my hola mundo changes

--- bin/app.js
+++ bin/app.js
@@ -1,16 +1,18 @@
-var vcap_services = JSON.parse(process.env.VCAP_SERVICES);
-// var vcap_services = JSON.parse('{"mongolab":[{"name":"gnavi-json","label":"mongolab","tags":["document","mongodb","Data Store"],"plan":"sandbox","credentials":{"uri":"mongodb://CloudFoundry_ids0og1r_dnb1iag3_2snsv6co:cVBkPdnzZr9TWePtVDIy0F-cQK1dgGEx@ds043220.mongolab.com:43220/CloudFoundry_ids0og1r_dnb1iag3"}}]}');
+// var vcap_services = JSON.parse(process.env.VCAP_SERVICES);
+var vcap_services = JSON.parse('{"mongolab":[{"name":"gnavi-json","label":"mongolab","tags":["document","mongodb","Data Store"],"plan":"sandbox","credentials":{"uri":"mongodb://CloudFoundry_ids0og1r_dnb1iag3_2snsv6co:cVBkPdnzZr9TWePtVDIy0F-cQK1dgGEx@ds043220.mongolab.com:43220/CloudFoundry_ids0og1r_dnb1iag3"}}]}');
 var uri = vcap_services.mongolab[0].credentials.uri;
 
 var collections = ["cursor", "gnavi"];
 var mongojs = require('mongojs');
 var db = mongojs(uri, collections);
 
+var request = require('request');
+
 var application_root = __dirname;
 var express = require("express");
-var request = require('request');
 var path = require("path");
 var app = express();
+
 var allowCrossDomain = function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
@@ -25,6 +27,9 @@ var allowCrossDomain = function(req, res, next) {
     }
 };
 
+// Config
+app.use(allowCrossDomain);
+app.use(express.static(path.join(application_root, "public")));
 
 function getNextStart(total_hit_count, hit_per_page, page_offset) {
   if (page_offset * hit_per_page >= total_hit_count)
@@ -64,56 +69,24 @@ function saveCursor(next_start) {
 
 };
 
-function isPercentage(jsonbody) {
-  var url = "http://api.gnavi.co.jp/ver1/RestSearchAPI/?keyid=23cf42cc2b30d584faae96e40544372e&pref=PREF13&hit_per_page=500&format=json";
-
-  request(url, function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-      res.set('Content-Type', 'application/json');
-      res.send(body);
-
-      var jsonbody = JSON.parse(body);
-
-      res.send(jsonbody.rest[0]);
-      for (var i in jsonbody.rest)
-      {
-        console.log(jsonbody.rest[i].name);  
-        db.gnavi.save(jsonbody.rest[i] , function(err, saved) {
-          if( err || !saved ) console.log("Rest not saved");
-          else console.log(jsonbody.rest[i].name_kana + " saved");
-        });
-      }
-    }
-    else
-    {
-      console.log(response.statusCode);
-      console.log(error);
-
-    }
-  });
-};
-
-// Config
-app.use(allowCrossDomain);
-app.use(express.static(path.join(application_root, "../client")));
 
 app.get('/callSaveGnavi', function (req, res) {
-  var url = "http://api.gnavi.co.jp/ver1/RestSearchAPI/?keyid=23cf42cc2b30d584faae96e40544372e&pref=PREF13&hit_per_page=500&format=json";
+  var url = "http://api.gnavi.co.jp/ver1/RestSearchAPI/?keyid=23cf42cc2b30d584faae96e40544372e&pref=PREF13&hit_per_page=5&format=json";
+  res.set('Content-Type', 'application/json');
 
   request(url, function (error, response, body) {
     if (!error && response.statusCode == 200) {
-      res.set('Content-Type', 'application/json');
+      // res.set('Content-Type', 'application/json');
       res.send(body);
 
       var jsonbody = JSON.parse(body);
 
-      res.send(jsonbody.rest[0]);
       for (var i in jsonbody.rest)
       {
         console.log(jsonbody.rest[i].name);  
         db.gnavi.save(jsonbody.rest[i] , function(err, saved) {
           if( err || !saved ) console.log("Rest not saved");
-          else console.log(jsonbody.rest[i].name_kana + " saved");
+          else console.log(i + ") " + jsonbody.rest[i].name_kana + " saved");
         });
       }
     }
@@ -127,7 +100,9 @@ app.get('/callSaveGnavi', function (req, res) {
 });
 
 
-
+app.get('/api', function (req, res) {
+  res.send('Our Sample API is up...');
+});
 
 
 app.listen(process.env.PORT || 9000);
